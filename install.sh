7ffe4ba9cef1bdff90ceea300224a9814dc2592a
2290142073@qq.com <2290142073@qq.com>
2020-07-19 19:39:26 +0800

    init

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ export LD_LIBRARY_PATH="/usr/local/cuda/lib64:${LD_LIBRARY_PATH}"
 
 cd ~ && git clone --recursive --branch v1.5.x https://github.com/apache/incubator-mxnet.git
 cd incubator-mxnet && git reset --hard 75a9e187d00a8b7ebc71412a02ed0e3ae489d91f
-# cd /usr/local/cuda/lib64 && sudo ln -s stubs/libcuda.so libcuda.so.1
+cd /usr/local/cuda/lib64 && sudo ln -s stubs/libcuda.so libcuda.so.1
 cd ~/mine/bytescheduler &&  /home/ubuntu/anaconda3/envs/mxnet_p36/bin/python setup.py install
 
